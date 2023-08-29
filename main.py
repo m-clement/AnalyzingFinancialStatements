a7144d17a5ffef51867cff2c48d8e759064232c8
Miguel Clement <miguelclement@gmail.com>
2023-08-29 15:52:56 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import matplotlib.pyplot as plt
+
+api_key = open('api_key.txt', 'r').read()
+
+ticker = input("Please enter a ticker symbol: ").upper()
+years = int(input("Please enter desired range in years: "))
+
+income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?Limit={years}&apikey={api_key}")
+income_statement = income_statement.json()
+
+revenues = list(reversed([income_statement[i]['revenue'] for i in range(len(income_statement))]))
+profits = list(reversed([income_statement[i]['grossProfit'] for i in range(len(income_statement))]))
+
+plt.plot(revenues, label="Revenue")
+plt.plot(profits, label="Profit")
+plt.title(f"${ticker} Revenue & Profit")
+plt.legend(loc="upper left")
+plt.show()
